e10c98758379ab0a882cb84b14ab1b95576eae48
Jacob <jmooree30@gmail.com>
2017-09-30 16:38:24 -0400

    Possible_moves working

--- queen.rb
+++ queen.rb
@@ -1,10 +1,10 @@
 class Queen
   attr_accessor :x_position, :y_position, :piece, :color, :moves
-  def initialize(position,is_white) 
+  def initialize(position,boolean) 
     @x_position = position[0]
     @y_position = position[1]
-    @piece = is_white ? "♛" : "♕"
-    @color = is_white ? "white" : "black"
+    @piece = boolean ? "♛" : "♕"
+    @color = boolean ? "white" : "black"
     @moves = [[+1,0],
     [-1,0],
     [0,+1],

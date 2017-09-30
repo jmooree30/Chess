020d97c75849298d86da595ad38d965f88b263d6
Jacob <jmooree30@gmail.com>
2017-09-30 15:35:15 -0400

    updated pawns possible moves

--- pawn.rb
+++ pawn.rb
@@ -1,28 +1,46 @@
 class Pawn
-	attr_accessor :x_position, :y_position, :piece, :color, :counter :moves
-	def initialize(position,boolean) 
-      @x_position = position[0]
-      @y_position = position[1]
-      @piece = boolean ? "♟" : "♙"
-      @color = boolean ? "white" : "black"
-      @counter = 0
-	  @moves = boolean ?  [[-1,0],[-1,-1],[-1,+1],[-2,0]] : [[+1,0],[+1,+1],[+1,-1],[+2,0]]
-    end 
+  attr_accessor :x_position, :y_position, :piece, :color, :counter, :moves
+  def initialize(position,boolean) 
+    @x_position = position[0]
+    @y_position = position[1]
+    @piece = boolean ? "♟" : "♙"
+    @color = boolean ? "white" : "black"
+    @counter = 0
+	@moves = boolean ?  [[-1,0],[-1,-1],[-1,+1],[-2,0]] : [[+1,0],[+1,+1],[+1,-1],[+2,0]]
+  end 
 
-    def possible_moves(board)
-      move_list = Array.new
-      @moves.each do |moves|
-        x = @x_position + moves[0] 
-        y = @y_position + moves[1]
+  def possible_moves(board)
+    move_list = Array.new
+    @moves.each_with_index do |moves,index|
+      x = @x_position + moves[0] 
+      y = @y_position + moves[1]
+      if index == 0 
         if x.between?(0,7) and y.between?(0,7)
           if board[x][y] == " "
-            move_list << [x,y]
-          elsif board[x][y].color != self.color
             move_list << [x,y]
           end
         end  
       end 
-      p move_list
+      if index.between?(1,2)
+        if x.between?(0,7) and y.between?(0,7)
+          if board[x][y] == " "
+          elsif board[x][y].color != self.color 
+          move_list << [x,y]
+          end
+        end 
+      end 
+      if index == 3
+      	if counter == 0
+      	  if x.between?(0,7) and y.between?(0,7)
+      	    if board[x][y] && board[x-1][y] == " "
+      	      move_list << [x,y] 
+      	    end
+      	  end 
+      	end  
+      end 
     end 
+    p move_list
+    @counter +=1
+  end 
 
 end
